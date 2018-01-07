9fefc9b7017c995a27bd9d39f73034b7ba91becc
Peter Fong <feilong7777@hotmail.com>
2018-01-07 01:22:31 -0800

    Initial Commit

---
+++ Whetstone/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Whetstone
+//
+//  Created by Peter Fong on 1/7/18.
+//  Copyright © 2018 Peter Fong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
