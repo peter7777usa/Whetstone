1b522479347e72271b1d85fd0ef2203738699ebd
Peter Fong <Peter_Fong@intuit.com>
2018-01-18 11:38:25 -0800

    wip

--- Whetstone/Graph/DijkstraShortestPath.swift
+++ Whetstone/Graph/DijkstraShortestPath.swift
@@ -18,31 +18,58 @@ class PathInfo: NSObject {
 
 class DijkstraShortestPath: NSObject {
     
-    var nodeDistanceTable = [GraphNode: [PathInfo]]()
+    var nodeDistanceTable = [GraphNode: [GraphNode: PathInfo]]()
     var graph = Graph()
     
-    func populateDistanceTable(node: GraphNode) {
-        var shortestKnownDest = [PathInfo]()
-        var pathInfo = PathInfo()
-        pathInfo.destinationNode = node
-        pathInfo.shortestDistance = 0
-        shortestKnownDest.append(pathInfo)
+    func populateShortestDistanceTable(node: GraphNode) {
         
+        var shortestKnownDest = [GraphNode: PathInfo]()
+        let initialPathInfo = PathInfo()
+        initialPathInfo.destinationNode = node
+        initialPathInfo.shortestDistance = 0
+        shortestKnownDest[node] = initialPathInfo
+        
+        var currentPathInfo = getNextClosestUnvisitedNode(shortestKnownDest: shortestKnownDest)
+        while currentPathInfo != nil {
+            
+            for connectedNode in (currentPathInfo?.destinationNode.connectedNodes)! {
+                let existingPathToConnectedNode = shortestKnownDest[connectedNode.node]
+                if  existingPathToConnectedNode == nil { //Add new Path into Table
+                    let newPathInfo = PathInfo()
+                    newPathInfo.destinationNode = connectedNode.node
+                    newPathInfo.shortestDistance = (shortestKnownDest[(currentPathInfo?.destinationNode)!]?.shortestDistance)! + connectedNode.weight
+                    newPathInfo.previousNode = (currentPathInfo?.destinationNode)!
+                    shortestKnownDest[newPathInfo.destinationNode] = newPathInfo
+                }else { //New Path Exist, update it if necessary
+                    let shortestDistanceRecordedWithCurrentConnectedNode = existingPathToConnectedNode?.shortestDistance
+                    let distanceToCurrentConnectedNode = (shortestKnownDest[(currentPathInfo?.destinationNode)!]?.shortestDistance)! + connectedNode.weight
+                    if shortestDistanceRecordedWithCurrentConnectedNode! > distanceToCurrentConnectedNode {
+                        existingPathToConnectedNode?.shortestDistance = distanceToCurrentConnectedNode
+                        existingPathToConnectedNode?.previousNode = (currentPathInfo?.destinationNode)!
+                    }
+                }
+            }
+            currentPathInfo = getNextClosestUnvisitedNode(shortestKnownDest: shortestKnownDest)
+        }
     }
     
-    func findShortestDistanceOnNode(shortestKnownDest: [PathInfo]) {
+    func getNextClosestUnvisitedNode(shortestKnownDest: [GraphNode: PathInfo]) -> PathInfo? {
         var shortestUnVisistedNode: PathInfo? = nil
-        for pathInfo in shortestKnownDest {
-            if pathInfo.visited == false && pathInfo.shortestDistance < shortestUnVisistedNode!.shortestDistance{
-                shortestUnVisistedNode = pathInfo
+        for pathInfo in shortestKnownDest.values {
+            if let unvisitedNode =  shortestUnVisistedNode {
+                if pathInfo.shortestDistance < unvisitedNode.shortestDistance {
+                     shortestUnVisistedNode = pathInfo
+                }
+            }else {
+                if pathInfo.visited == false {
+                    shortestUnVisistedNode = pathInfo
+                }
             }
         }
+        return shortestUnVisistedNode
+    }
+    
+    func calculateDistanceToAdjacentNodesWithNode(pathInfo: PathInfo) {
         
-//        let smallestDistance = node.connectedNodes.first?.weight
-//        for node in node.connectedNodes {
-//            if smallestDistance! > node.weight {
-//                smallestDistance == node.weight
-//            }
-//        }
     }
 }
